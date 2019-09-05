0b0926e0e4397aeaa768e08db09142e7b558e22d
Chris Cynarski <krzysztof@cynarski.net>
2019-09-05 17:16:09 +0100

    Initial version of Audit Package without DB dependency.

--- src/Listeners/AuditEventListener.php
+++ src/Listeners/AuditEventListener.php
@@ -2,16 +2,15 @@
 
 namespace Fnp\Audit\Listeners;
 
-use Carbon\Carbon;
-use Fnp\Audit\Jobs\StoreAuditJob;
+use Fnp\Audit\Contracts\Auditable;
+use Fnp\Audit\Events\NewAuditEntry;
 use Fnp\Audit\Registry\AuditEventRegistry;
-use Fnp\Audit\Services\AuditService;
 use Fnp\Dto\Common\Flags\DtoToArrayFlags;
 use Fnp\Dto\Common\Helper\Iof;
 use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Config;
+use ReflectionClass;
+use ReflectionProperty;
 
 class AuditEventListener
 {
@@ -31,16 +30,13 @@ class AuditEventListener
     }
 
     /**
-     * Handle the event.
+     * Default handler for Subscribed Events
      *
-     * @param string $handle
-     * @param object $event
+     * @param object|null $event
      *
-     * @return void
      * @throws \ReflectionException
-     * @throws \Exception
      */
-    public function handle($handle, $event = NULL)
+    public function handle($event = NULL)
     {
         if (is_array($event))
             $event = $event[0];
@@ -48,65 +44,74 @@ class AuditEventListener
         if (!is_object($event))
             return;
 
-        $eventClass = get_class($event);
+        $eventClass  = get_class($event);
+        $eventHandle = AuditEventRegistry::getHandle($eventClass);
 
         if (!AuditEventRegistry::hasClass($eventClass))
             return;
 
-        $payload              = new Collection();
-        $reflectionClass      = new \ReflectionClass($event);
-        $reflectionProperties = $reflectionClass->getProperties(\ReflectionProperty::IS_PUBLIC);
-        $eventHandle          = AuditEventRegistry::getHandle($eventClass);
+        if ($event instanceof Auditable)
+            $payload = new Collection($event->auditPayload());
+        else
+            $payload = $this->extractPayloadFromEvent($event);
+
+        NewAuditEntry::dispatch($eventHandle, $payload);
+    }
+
+    /**
+     * Extract Payload From the Event Object
+     *
+     * @param $event
+     *
+     * @return Collection
+     * @throws \ReflectionException
+     */
+    private function extractPayloadFromEvent($event)
+    {
+        $reflectionClass      = new ReflectionClass($event);
+        $reflectionProperties = $reflectionClass->getProperties(ReflectionProperty::IS_PUBLIC);
 
         /*
          * If event is serializable then just take the data.
-         * Property serialization otherwise
          */
-        if (Iof::arrayable($event)) {
-            $payload = new Collection($event->toArray());
-        } else {
-            foreach ($reflectionProperties as $reflectionProperty) {
+        if (Iof::arrayable($event))
+            /** @noinspection PhpUndefinedMethodInspection */
+            return new Collection($event->toArray());
 
-                $propertyName  = $reflectionProperty->getName();
-                $propertyValue = $reflectionProperty->getValue($event);
+        $payload = new Collection();
 
-                /*
-                 * Ignore properties with no value
-                 */
-                if (is_null($propertyValue))
-                    continue;
+        /*
+         * Otherwise get all the public properties
+         */
+        foreach ($reflectionProperties as $reflectionProperty) {
 
-                /*
-                 * Serialize the properties
-                 */
-                if (is_object($propertyValue) && Iof::stringable($propertyValue)) {
-                    $payload->put('_' . $propertyName, $propertyValue->__toString());
-                } elseif (is_object($propertyValue) && Iof::arrayable($propertyValue)) {
-                    $payload->put('&' . $propertyName, $propertyValue->toArray(
-                        DtoToArrayFlags::EXCLUDE_NULLS +
-                        DtoToArrayFlags::SERIALIZE_STRING_PROVIDERS +
-                        DtoToArrayFlags::PREFER_STRING_PROVIDERS
-                    ));
-                } elseif (is_object($propertyValue)) {
-                    $payload->put('@' . $propertyName, get_object_vars($propertyValue));
-                } else {
-                    $payload->put('$' . $propertyName, $propertyValue);
-                }
+            $propertyName  = $reflectionProperty->getName();
+            $propertyValue = $reflectionProperty->getValue($event);
+
+            /*
+             * Ignore properties with no value
+             */
+            if (is_null($propertyValue))
+                continue;
+
+            /*
+             * Serialize the properties
+             */
+            if (is_object($propertyValue) && Iof::stringable($propertyValue)) {
+                $payload->put($propertyName, $propertyValue->__toString());
+            } elseif (is_object($propertyValue) && Iof::arrayable($propertyValue)) {
+                $payload->put($propertyName, $propertyValue->toArray(
+                    DtoToArrayFlags::EXCLUDE_NULLS +
+                    DtoToArrayFlags::SERIALIZE_STRING_PROVIDERS +
+                    DtoToArrayFlags::PREFER_STRING_PROVIDERS
+                ));
+            } elseif (is_object($propertyValue)) {
+                $payload->put($propertyName, get_object_vars($propertyValue));
+            } else {
+                $payload->put($propertyName, $propertyValue);
             }
         }
 
-        $ip   = AuditService::requestIpAddress($this->request);
-        $when = Carbon::now();
-        Auth::check();
-
-        StoreAuditJob::dispatch(
-            $eventHandle,
-            $when,
-            AuditService::session(),
-            $ip,
-            $payload->toArray(),
-            AuditService::requestDetails($this->request),
-            optional(Auth::getUser())->id
-        )->onQueue(Config::get('audit.queue'));
+        return $payload;
     }
 }
